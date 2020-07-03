730c240b42e6b123ee8cd422e1bd290d1de58eec
songhao1 <717977831@qq.com>
2020-07-03 23:11:09 +0800

    2020/7/3

--- 源.cpp
+++ 源.cpp
@@ -423,3 +423,140 @@
 //	printf("sum=%d\n", sum);
 //	return 0;
 //}
+
+
+//P40
+//int main() {
+//	int a[10] = { 5,3,2,6,8,4 };
+//	for (int i = 0; i < 10; i++) {
+//		printf("a[%d]=%d\n", i, a[i]);
+//	}
+//	return 0;
+//}
+
+
+//P40
+//int main() {
+//	int a[10];
+//	scanf("%d", &a[0]);
+//
+//	for (int i = 1; i < 10; i++) {
+//		a[i] = a[i - 1] * 2;
+//	}
+//
+//	for (int i = 0; i < 10; i++) {
+//		printf("a[%d]=%d\n", i, a[i]);
+//	}
+//	return 0;
+//}
+
+
+//P42
+//int main() {
+//	int a = 1, b = 2;
+//	printf("a=%d,b=%d\n", a, b);
+//	int temp = a;
+//	a = b;
+//	b = temp;
+//	printf("a=%d,b=%d\n", a, b);
+//	return 0;
+//}
+
+
+//P43
+//int main() {
+//	int a[5] = { 3,1,4,5,2 };
+//	for (int i = 1; i <= 4; i++) {
+//		for (int j = 0; j < 5 - i; j++) {
+//			if (a[j] > a[j + 1]) {
+//				int temp = a[j];
+//				a[j] = a[j + 1];
+//				a[j + 1] = temp;
+//			}
+//		}
+//	}
+//
+//	for (int i = 0; i < 5; i++) {
+//		printf("%d ", a[i]);
+//	}
+//	return 0;
+//}
+
+
+//P44
+//int main() {
+//	int a[5][6] = { {3,1,2},{8,4},{},{1,2,3,4,5} };
+//	for (int i = 0; i < 5; i++) {
+//		for (int j = 0; j < 6; j++) {
+//			printf("%d ", a[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//P44
+//int main() {
+//	int a[3][3], b[3][3];
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			scanf("%d", &a[i][j]);
+//		}
+//	}
+//
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			scanf("%d", &b[i][j]);
+//		}
+//	}
+//
+//	int c[3][3];
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			c[i][j] = a[i][j] + b[i][j];
+//		}
+//	}
+//
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			printf("%d ", c[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//P45
+//int a[1000000];
+//int main() {
+//	for (int i = 0; i < 1000000; i++) {
+//		a[i] = i;
+//		printf("a[%d]=%d\n", i, a[i]);
+//	}
+//	return 0;
+//}
+
+
+//P46
+//int main() {
+//	int a[3][3][3];
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			for (int k = 0; k < 3; k++) {
+//				scanf("%d", &a[i][j][k]);
+//				a[i][j][k]++;
+//			}
+//		}
+//	}
+//
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			for (int k = 0; k < 3; k++) {
+//				printf("%d\n", a[i][j][k]);
+//			}
+//		}
+//	}
+//	return 0;
+//}
